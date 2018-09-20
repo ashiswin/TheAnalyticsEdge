1876b9ecb03c85ce23bf70828d379553eb8ad4d6
Isaac Ashwin <ashiswin@gmail.com>
2018-09-20 13:51:40 +0800

    Added week 2

---
+++ Week 2/Predicting the Quality and Price of Wine/WineAnalysis.R
@@ -0,0 +1,49 @@
+setwd('/Users/ashiswin/Documents/School/Analytics Edge/Week 2')
+wine <- read.csv("wine.csv")
+str(wine)
+summary(wine)
+
+wine[wine$VINT == 1966,]
+
+plot(wine$VINT, wine$LPRICE)
+pairs(wine)
+
+# Make my training and testing set
+winetrain <- subset(wine, wine$VINT <= 1978 & !is.na(wine$LPRICE)) # Remove all NA items
+winetest <- subset(wine, wine$VINT > 1978)
+
+m1 <- lm(LPRICE ~ WRAIN, winetrain)
+m1a <- lm(LPRICE ~ WRAIN + VINT, winetrain)
+m2 <- lm(LPRICE ~ VINT + WRAIN + DEGREES + HRAIN, winetrain)
+
+summary(m1)
+summary(m2)
+summary(m1a)
+
+plot(wine$VINT, wine$LPRICE)
+abline(m1)
+
+# Get correlation between variables in wine set
+cor(winetrain)
+
+# Get coefficients from the model
+m2$coefficients
+
+# Get residuals from model
+m2$residuals
+
+# Lets get R^2 manually
+ssttrain <- sum((winetrain$LPRICE - mean(winetrain$LPRICE)) ^ 2)
+ssetrain <- sum(m2$residuals ^ 2)
+
+rsquared <- 1 - (ssetrain / ssttrain)
+
+# Perform a prediction into the future
+wineprediction <- predict(m2, newdata = winetest)
+wineprediction
+winetest$LPRICE
+
+# Calculate R^2 of the prediction
+ssetest <- sum((wineprediction[1:2] - winetest$LPRICE[1:2]) ^ 2)
+ssttest <- sum((winetest$LPRICE[1:2] - mean(winetrain$LPRICE)) ^ 2)
+rsquaredtest <- 1 - (ssetest / ssttest)
